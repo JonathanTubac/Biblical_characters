16fadccd2604b30450b45a14c721a7c9e6c49448
JonathanTubac <javiertubac1290.e@gmail.com>
2026-07-29 16:37:07 -0600

    feat: Added data and component menu

---
+++ src/data/characters.js
@@ -0,0 +1,20 @@
+[
+    {
+        genesis: [
+            {
+                personaje: "Adan",
+                desc: "",
+                pistas: {
+                    facil: [1, 2, 3],
+                    intermedio: [4, 5, 6],
+                    dificil: [7, 8, 9]
+                },
+            },
+
+            {
+                
+            }
+        ]
+    }
+
+]
