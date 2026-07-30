539530d4ea8eea6236f72bebe5fc652a762e5aca
JonathanTubac <javiertubac1290.e@gmail.com>
2026-07-29 18:50:01 -0600

    feat: Added new fonts and Menu styles

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,11 @@
+import './Menu.css'
+
 export default function Menu() {
     return (
-        <section id="menu">
-            
+        <section id="menu" className="menu">
+            <h2>Selecciona una opción</h2>
+            <button>Jugar</button>
+            <button>Aprender</button>
         </section>
     )
 }
